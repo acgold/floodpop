bf756c7193046ea51c205031a3fc44d3c9d0ceab
Adam Gold <47038242+acgold@users.noreply.github.com>
2025-03-27 23:58:12 -0400

    Cleaned up validation workflow and outputs

--- code/building_classification_validation.R
+++ code/building_classification_validation.R
@@ -13,9 +13,9 @@ data_path = "your_path"
 ############### Functions ##################
 # Compute precision, recall, and F1 score for each class
 calculate_metrics <- function(class_label, df) {
-  true_positive <- sum(df$res_or_not == class_label & df$res_or_not_1 == class_label)
-  false_positive <- sum(df$res_or_not != class_label & df$res_or_not_1 == class_label)
-  false_negative <- sum(df$res_or_not == class_label & df$res_or_not_1 != class_label)
+  true_positive <- sum(df$val_res_or_not == class_label & df$res_or_not == class_label)
+  false_positive <- sum(df$val_res_or_not != class_label & df$res_or_not == class_label)
+  false_negative <- sum(df$val_res_or_not == class_label & df$res_or_not != class_label)
   
   precision <- ifelse(true_positive + false_positive > 0, true_positive / (true_positive + false_positive), 0)
   recall <- ifelse(true_positive + false_negative > 0, true_positive / (true_positive + false_negative), 0)
@@ -32,26 +32,27 @@ city_results <- foreach(i = cities, .combine = "bind_rows") %do% {
   
   b_validation <- sf::st_read(file.path(results_path, "validation/sj_cities.gdb"), layer = paste0(i,"_validation"))
   
-  b_buildings <- sf::st_read(file.path(results_path, "validation/buildings_cities.gdb"), layer=  paste0(i,"_buildings")) %>%
+  b_buildings <- sf::st_read(file.path(results_path, "validation/buildings_w_lu.gdb"), layer=  paste0(i,"_buildings_w_lu")) %>%
     as_tibble()
   
   b_validation_df <- b_validation %>% 
     as_tibble() %>% 
-    mutate(res_or_not = replace_na(res_or_not, -1),
-           res_or_not_1 = replace_na(res_or_not_1, -1)) %>% 
-    mutate(res_or_not = ifelse(res_or_not_1 == -1, -1, res_or_not),
-           res_or_not_1 = ifelse(res_or_not == -1, -1, res_or_not_1))
+    mutate(val_res_or_not = replace_na(val_res_or_not, -1),
+           res_or_not = replace_na(res_or_not, -1)) %>% 
+    mutate(val_res_or_not = ifelse(res_or_not == -1, -1, val_res_or_not),
+           res_or_not = ifelse(val_res_or_not == -1, -1, res_or_not))
   
   # parcel count
   parcel_count <- b_validation_df %>% 
-    group_by(res_or_not) %>% 
-    summarize(parcel_count = n())
+    group_by(val_res_or_not) %>% 
+    summarize(parcel_count = n()) %>% 
+    rename(res_or_not = val_res_or_not)
   
   # building count
   building_count <- b_buildings %>% 
     group_by(res_or_not) %>% 
     summarize(building_count = n(),
-              building_area = sum(Shape_Area)/1000000)
+              building_area = sum(SHAPE_Area)/1000000)
   
   # parcel and building count + area
   val_count_area <- parcel_count %>% 
@@ -60,7 +61,7 @@ city_results <- foreach(i = cities, .combine = "bind_rows") %do% {
   
   
   # Get all unique classes from both columns
-  unique_classes <- union(unique(b_validation_df$res_or_not), unique(b_validation_df$res_or_not_1))
+  unique_classes <- union(unique(b_validation_df$val_res_or_not), unique(b_validation_df$res_or_not))
   
   # Compute metrics for each unique class
   metrics <- map_dfr(unique_classes, calculate_metrics, df = b_validation_df) %>% 
@@ -107,31 +108,34 @@ more_city_results %>%
 # Miami
 m_blocks <- sf::st_read(file.path(results_path, "validation/validation_blocks.gdb"), layer = "miami_blocks")
 
-m_buildings <- sf::st_read(file.path(results_path, "validation/buildings_cities.gdb"), layer=  "miami_buildings_w_lu") %>%
+m_buildings <- sf::st_read(file.path(results_path, "validation/buildings_w_lu.gdb"), layer=  "miami_buildings_w_lu") %>%
   as_tibble()
 
 counts_by_block_miami <- m_buildings %>%
   group_by(GEOID20) %>% 
-  filter(all(LU %in% c(10,11,13))) %>% 
+  filter(all(landuse %in% c(10,11,13))) %>% 
   filter(res_or_not == 1) %>% 
   group_by(GEOID20) %>% 
-  summarize(n = n(), area =sum(Shape_Area)) %>% 
+  summarize(n = n(), area =sum(SHAPE_Area)) %>% 
   left_join(m_blocks %>%
-              select(GEOID20, H1_001N, H1_002N, tot_hu_either_sfha), by = c("GEOID20" = "GEOID20")) 
+              select(geoid20, p1_001n, h1_001n, h1_002n, tot_hu_either_sfha), by = c("GEOID20" = "geoid20")) 
 
-m_eq <- lm(n ~ H1_001N, data = counts_by_block_miami)
+m_eq <- lm(n ~ h1_001n, data = counts_by_block_miami)
 summary(m_eq)
 
-max_limit <- max(max(counts_by_block_miami$H1_001N, na.rm = TRUE) + 1, 
+max_limit <- max(max(counts_by_block_miami$h1_001n, na.rm = TRUE) + 1, 
                  max(counts_by_block_miami$n, na.rm = TRUE) + 1)
 
 m_building_count <- scales::comma(counts_by_block_miami %>% pull(n) %>% sum())
-m_unit_count <- scales::comma(counts_by_block_miami %>% pull(H1_001N) %>% sum())
+m_unit_count <- scales::comma(counts_by_block_miami %>% pull(h1_001n) %>% sum())
 m_blocks_count <- scales::comma(counts_by_block_miami %>% nrow())
+
+m_buildings %>% filter(res_or_not == 1) %>% group_by(GEOID20) %>% count()
+
 
 miami_val_sf <- counts_by_block_miami %>%
   ggplot() +
-  geom_hex(aes(x = H1_001N, y = n), bins = 50) +
+  geom_hex(aes(x = h1_001n, y = n), bins = 50) +
   geom_abline(aes(slope = 1, intercept = 0), lwd = 0.15, alpha = 0.5) +
   theme_bw() +
   coord_equal() +
@@ -164,31 +168,34 @@ miami_val_sf <- counts_by_block_miami %>%
 # Meck
 meck_blocks <- sf::st_read(file.path(results_path, "validation/validation_blocks.gdb"), layer = "meck_blocks")
 
-meck_buildings <- sf::st_read(file.path(results_path, "validation/buildings_cities.gdb"), layer=  "meck_buildings_w_lu") %>%
+meck_buildings <- sf::st_read(file.path(results_path, "validation/buildings_w_lu.gdb"), layer=  "meck_buildings_w_lu") %>%
   as_tibble()
 
 counts_by_block_meck <- meck_buildings %>%
   group_by(GEOID20) %>% 
-  filter(all(descproper %in% c("Single-Family"))) %>% 
+  filter(all(landuse %in% c("Single-Family"))) %>% 
   filter(res_or_not == 1) %>% 
   group_by(GEOID20) %>% 
-  summarize(n = n(), area =sum(Shape_Area)) %>% 
+  summarize(n = n(), area =sum(SHAPE_Area)) %>% 
   left_join(meck_blocks %>%
-              select(GEOID20, P1_001N, H1_001N, H1_002N, tot_hu_either_sfha), by = c("GEOID20" = "GEOID20")) 
+              select(geoid20, p1_001n, h1_001n, h1_002n, tot_hu_either_sfha), by = c("GEOID20" = "geoid20")) 
 
-meck_eq <- lm(n ~ H1_001N, data = counts_by_block_meck)
+meck_eq <- lm(n ~ h1_001n, data = counts_by_block_meck)
 summary(meck_eq)
 
-max_limit <- max(max(counts_by_block_meck$H1_001N, na.rm = TRUE) + 1, 
+max_limit <- max(max(counts_by_block_meck$h1_001n, na.rm = TRUE) + 1, 
                  max(counts_by_block_meck$n, na.rm = TRUE) + 1)
 
 meck_building_count <- scales::comma(counts_by_block_meck %>% pull(n) %>% sum())
-meck_unit_count <- scales::comma(counts_by_block_meck %>% pull(H1_001N) %>% sum())
+meck_unit_count <- scales::comma(counts_by_block_meck %>% pull(h1_001n) %>% sum())
 meck_blocks_count <- scales::comma(counts_by_block_meck %>% nrow())
+
+# 
+meck_buildings %>% filter(res_or_not == 1) %>% group_by(GEOID20) %>% count()
 
 meck_val_sf <- counts_by_block_meck %>%
   ggplot() +
-  geom_hex(aes(x = H1_001N, y = n), bins = 50) +
+  geom_hex(aes(x = h1_001n, y = n), bins = 50) +
   geom_abline(aes(slope = 1, intercept = 0), lwd = 0.15, alpha = 0.5) +
   theme_bw() +
   coord_equal() +
@@ -221,31 +228,33 @@ meck_val_sf <- counts_by_block_meck %>%
 # Sacramento
 sac_blocks <- sf::st_read(file.path(results_path, "validation/validation_blocks.gdb"), layer = "sacramento_blocks")
 
-sac_buildings <- sf::st_read(file.path(results_path, "validation/buildings_cities.gdb"), layer=  "sacramento_buildings_w_lu") %>%
+sac_buildings <- sf::st_read(file.path(results_path, "validation/buildings_w_lu.gdb"), layer=  "sacramento_buildings_w_lu") %>%
   as_tibble()
 
 counts_by_block_sac <- sac_buildings %>%
   group_by(GEOID20) %>% 
-  filter(all(LU_GENERAL %in% c("Residential") & LU_SPECIF %in% c("Single Family"))) %>% 
+  filter(all(landuse %in% c("Residential") & sec_landuse %in% c("Single Family"))) %>% 
   filter(res_or_not == 1) %>% 
   group_by(GEOID20) %>% 
-  summarize(n = n(), area =sum(Shape_Area)) %>% 
+  summarize(n = n(), area =sum(SHAPE_Area)) %>% 
   left_join(sac_blocks %>%
-              select(GEOID20, P1_001N, H1_001N, H1_002N, tot_hu_either_sfha), by = c("GEOID20" = "GEOID20")) 
+              select(geoid20, p1_001n, h1_001n, h1_002n, tot_hu_either_sfha), by = c("GEOID20" = "geoid20")) 
 
-sac_eq <- lm(n ~ H1_001N, data = counts_by_block_sac)
+sac_eq <- lm(n ~ h1_001n, data = counts_by_block_sac)
 summary(sac_eq)
 
-max_limit <- max(max(counts_by_block_sac$H1_001N, na.rm = TRUE) + 1, 
+max_limit <- max(max(counts_by_block_sac$h1_001n, na.rm = TRUE) + 1, 
                  max(counts_by_block_sac$n, na.rm = TRUE) + 1)
 
 sac_building_count <- scales::comma(counts_by_block_sac %>% pull(n) %>% sum())
-sac_unit_count <- scales::comma(counts_by_block_sac %>% pull(H1_001N) %>% sum())
+sac_unit_count <- scales::comma(counts_by_block_sac %>% pull(h1_001n) %>% sum())
 sac_blocks_count <- scales::comma(counts_by_block_sac %>% nrow())
+
+sac_buildings %>% filter(res_or_not == 1) %>% group_by(GEOID20) %>% count()
 
 sac_val_sf <- counts_by_block_sac %>%
   ggplot() +
-  geom_hex(aes(x = H1_001N, y = n), bins = 50) +
+  geom_hex(aes(x = h1_001n, y = n), bins = 50) +
   geom_abline(aes(slope = 1, intercept = 0), lwd = 0.15, alpha = 0.5) +
   theme_bw() +
   coord_equal() +
@@ -273,7 +282,7 @@ sac_val_sf <- counts_by_block_sac %>%
   annotate("text", x = -Inf, y = max_limit * 0.9, label = paste0("   ", sac_blocks_count, " blocks"), size = 2, hjust = 0)+
   annotate("text", x = -Inf, y = max_limit * 0.8, label = paste0("   ", sac_unit_count," units"), size = 2, hjust = 0)+
   annotate("text", x = -Inf, y = max_limit * 0.7, label = paste0("   ", sac_building_count," buildings"), size = 2, hjust = 0)+
-  ggtitle("Sacramento, CA")
+  ggtitle("Sacramento County, CA")
 
 
 sf_validation_plot <-  meck_val_sf + miami_val_sf +sac_val_sf + plot_annotation(tag_levels = "a", tag_suffix = ")") + plot_layout(ncol=3, axes = "collect", guides = "auto")
@@ -284,36 +293,36 @@ ggsave(file.path(results_path, "figures/validation_buildings.png"), sf_validatio
 # Doubles
 counts_by_block_dub_sac <- sac_buildings %>%
   group_by(GEOID20) %>% 
-  filter(all(LU_GENERAL %in% c("Residential") & LU_SPECIF %in% c("Two Family"))) %>% 
+  filter(all(landuse %in% c("Residential") & sec_landuse %in% c("Two Family"))) %>% 
   filter(res_or_not == 1) %>% 
   group_by(GEOID20) %>% 
-  summarize(n = n(), area =sum(Shape_Area)) %>% 
+  summarize(n = n(), area =sum(SHAPE_Area)) %>% 
   left_join(sac_blocks %>%
-              select(GEOID20, H1_001N, H1_002N, tot_hu_either_sfha), by = c("GEOID20" = "GEOID20")) %>% 
-  mutate(Location = "Sacramento,\nCA")
+              select(geoid20, h1_001n, h1_002n, tot_hu_either_sfha), by = c("GEOID20" = "geoid20")) %>% 
+  mutate(Location = "Sacramento\nCounty, CA")
 
 counts_by_block_dub_miami <- m_buildings %>%
   group_by(GEOID20) %>% 
-  filter(all(LU %in% c(20))) %>% 
+  filter(all(landuse %in% c(20))) %>% 
   filter(res_or_not == 1) %>%
   group_by(GEOID20) %>% 
   summarize(n = n()) %>% 
   left_join(m_blocks %>%
-              select(GEOID20, H1_001N, H1_002N, tot_hu_either_sfha), by = c("GEOID20" = "GEOID20")) %>% 
-  filter(H1_001N < 100) %>% 
+              select(geoid20, h1_001n, h1_002n, tot_hu_either_sfha), by = c("GEOID20" = "geoid20")) %>% 
+  filter(h1_001n < 100) %>% 
   mutate(Location = "Miami-Dade\nCounty, FL")
 
-eq <- lm(n ~ H1_001N, data = counts_by_block_dub_miami)
+eq <- lm(n ~ h1_001n, data = counts_by_block_dub_miami)
 summary(eq)
 
-eq <- lm(n ~ H1_001N, data = counts_by_block_dub_sac)
+eq <- lm(n ~ h1_001n, data = counts_by_block_dub_sac)
 summary(eq)
 
 
 sac_val_dub_sf <- counts_by_block_dub_sac %>%
   bind_rows(counts_by_block_dub_miami) %>% 
   ggplot() +
-  geom_point(aes(x = H1_001N, y = n, color = Location), alpha = 1, pch = 1) +
+  geom_point(aes(x = h1_001n, y = n, color = Location), alpha = 1, pch = 1) +
   geom_abline(aes(slope = 0.5, intercept = 0)) +
   theme_bw() +
   coord_equal() +
